3e8409f9844c06abb58a68eccfca8bb41d745e13
Cassio Oliveira Silva <cos@vortx.com.br>
2021-09-28 08:41:33 -0300

    added SSG in home page to get price value

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 import { stripe } from '../service/stripe';
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps  = async () => {
   const price = await stripe.prices.retrieve('price_1Jdy7hGlTG1cEOOqpDcWrAUo')
 
   const product = {
@@ -48,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    } 
+    }, 
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
