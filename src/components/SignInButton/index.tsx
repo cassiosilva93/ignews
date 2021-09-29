7495fdbb7a28ef670829ecb8629915a2f1e52a41
Cassio Oliveira Silva <cos@vortx.com.br>
2021-09-29 08:38:02 -0300

    added oauth github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,25 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
-import styles from './styles.module.scss'
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signout, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button 
-      type="button"
-      className={styles.signInButton}
-    >
-      <FaGithub color="#04d361"/>
-      Cassio Oliveira Silva
-      <FiX color="#737380" className={styles.closeIcon}/>
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={() => signout()}>
+      <FaGithub color="#04d361" />
+        {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button 
+    <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn("github")}
     >
-      <FaGithub color="#eba417"/>
+      <FaGithub color="#eba417" />
       Sign in with Github
     </button>
-  )
+  );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { Provider as NextAuthProvider } from "next-auth/client";
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
