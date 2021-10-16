ea6eb85300ee6893fb2c91799432cecd598995d5
Cassio Oliveira Silva <cos@vortx.com.br>
2021-10-16 18:03:39 -0300

    added preview page and added your own style

---
+++ src/pages/posts/preview/[slug].tsx
@@ -0,0 +1,87 @@
+import Head from "next/head";
+import Link from 'next/link';
+import { GetStaticProps } from "next"
+import { RichText } from "prismic-dom";
+import { getPrismicClient } from "../../../service/prismic";
+import { useSession } from "next-auth/client";
+import { useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
+import styles from '../post.module.scss';
+
+interface PostPreviewProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function PostPreview({ post }: PostPreviewProps) {
+  const [session] = useSession();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (session?.activeSubscription) {
+      push(`/posts/${post.slug}`)
+    }
+  }, [post.slug, push, session])
+
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div 
+            className={`${styles.postContent} ${styles.previewContent}`}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link href="/">
+              <a>Subscribe now 🤗</a>
+            </Link>
+          </div>
+        </article>
+      </main>
+    </>
+  )
+}
+
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {})
+  const dateFormatted = new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric'
+  })
+
+  const post = { 
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
+    updatedAt: dateFormatted
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
+}
